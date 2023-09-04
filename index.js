68002e7a71c9dfe3408d5bf3ee055bf8ece2385d
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-09-04 02:16:20 -0400

    Made it so that players can join the same match using a code

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-const express = require("express")
-const app = express()
+// Make this a map
+let activeGames = new Map()
 
-app.get("/api", (req, res) => {
-    res.json({"test": [1, 2, 3]})
+function getMultiplayerId(length) {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+    let cLength = chars.length
+    let result = ""
+    for (let i = 0; i < length; i++) {
+        result += chars[Math.floor(Math.random() * cLength)]
+    }
+
+    if (activeGames.has(result)) {
+        result = getMultiplayerId(length)
+    }
+
+    return result
+}
+
+const io = require("socket.io")(5000, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
 })
+ 
+io.on("connection", (socket) => {
+    socket.on("multiplayer-started", (rules, sendCode) => {
+        let roomId = getMultiplayerId(6)
+        activeGames.set(roomId, rules)
+        socket.join(roomId)
+        sendCode(roomId)
+    })
 
-app.listen(5000, () => {
-    console.log("Server started on port 5000")
+    socket.on("join-multiplayer", (gameCode, validateGame) => {
+        const connectedSockets = io.sockets.adapter.rooms.get(gameCode)
+        const socketRooms = Array.from(socket.rooms.values()).filter((room) => room !== socket.id)
+
+        if (!connectedSockets) {
+            validateGame(false, "No game found")
+        } else if (socketRooms.length > 0 || connectedSockets.size >= 2) {
+            validateGame(false, "Game full")
+        } else {
+            socket.join(gameCode)
+            socket.to(gameCode).emit("opponent-joined")
+            validateGame(true, "", activeGames.get(gameCode))
+        }
+    })
+
+    socket.on("multiplayer-left", () => {
+        console.log("HEY HEY!")
+    })
 })
