54fa2eaa7b20f288f8877e357b0291895cfef3bb
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-09-08 02:40:46 -0400

    Added express signup post request

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const socketio = require("./socket.js")
 const express = require('express')
 const http = require('http')
+const cors = require('cors')
 
 const app = express()
+app.use(cors({
+    origin: "http://localhost:3000"
+}), express.json())
+
 const server = http.createServer(app)
 
 const io = socketio.getIo(server)
 
-// Express routes go here
+app.post("/api/signup", (req, res) => {
+    const {username, password} = req.body
+    console.log(username, password)
+    res.status(200).json({isValid: true})
+})
 
 server.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+    console.log('Server is running on port 5000')
+})
