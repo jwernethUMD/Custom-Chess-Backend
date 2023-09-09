404467ecb692614442083eec23867aecbaef1049
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-09-09 04:26:47 -0400

    Added mongoose

--- index.js
+++ index.js
@@ -1,21 +1,74 @@
 const socketio = require("./socket.js")
+const schemas = require("./schemas.js")
 const express = require('express')
 const http = require('http')
 const cors = require('cors')
+const bcrypt = require("bcryptjs")
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+const User = schemas.Users
+const dbUsername = process.env.DB_USERNAME
+const dbPassword = process.env.DB_PASSWORD
+const dbUri = `mongodb+srv://${dbUsername}:${dbPassword}@custom-chess.s847jfs.mongodb.net/custom-chess-users?retryWrites=true&w=majority`
+
+const saltRounds = 10
+
+mongoose.connect(dbUri)
 
 const app = express()
 app.use(cors({
     origin: "http://localhost:3000"
 }), express.json())
 
+function getPasswordHash(password) {
+    let hashedPassword
+    bcrypt.genSalt(saltRounds, function (saltError, salt) {
+        if (saltError) {
+            throw saltError
+        } else {
+        bcrypt.hash(password, salt, function(hashError, hash) {
+            if (hashError) {
+                throw hashError
+            } else {
+                hashedPassword = hash
+            }
+            })
+        }
+    })
+
+    return hashedPassword
+}
+
 const server = http.createServer(app)
 
 const io = socketio.getIo(server)
 
-app.post("/api/signup", (req, res) => {
+app.post("/api/signup", async (req, res) => {
     const {username, password} = req.body
-    console.log(username, password)
-    res.status(200).json({isValid: true})
+    let isValid = false
+    // Check if username is already in database
+    const existUsername = await User.findOne({ username: username})
+    if (existUsername) {
+        console.log('username taken')
+    } else {
+        // Create new account with username/password combo
+        const newUser = new User({
+            username: username,
+            password: password,
+            wins: 2,
+            losses: 3,
+            draws: 5
+        })
+        
+        const userSaved = await newUser.save()
+        
+        if (userSaved) {
+            isValid = true
+        }
+    }
+
+    res.status(200).json({isValid: isValid})
 })
 
 server.listen(5000, () => {
