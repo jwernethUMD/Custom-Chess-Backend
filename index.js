e20c2596d853619a38b5986ffe2b39771c67fe00
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-09-14 11:12:37 -0400

    Updated cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express()
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "https://custom-chess-jw.vercel.app",
+    origin: ["https://custom-chess-jw.vercel.app", "http://localhost:3000", "*"],
     credentials: true
 }), express.json())
 
