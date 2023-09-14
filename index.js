356909225ebb95d8fa31bcadea82d5d870296930
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-09-14 01:09:46 -0400

    Added vercel cors origin

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(
     })
 )
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://custom-chess-jw.vercel.app/"],
     credentials: true
 }), express.json())
 
