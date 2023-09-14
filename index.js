2b6a4b73f3ff7c77df87ada87f504c2aa25b7022
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-09-14 02:47:10 -0400

    Changed domain

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(
         saveUninitialized: false,
         cookie: {
            secure: true,
-           domain: "https://custom-chess-jw.vercel.app",
+           domain: "custom-chess-jw.vercel.app",
            sameSite: "none",
            maxAge: 1000 * 60 * 60 * 24
         }
